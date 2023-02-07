e0e8005aa35e74f82cce4fc6aa180754907e70f7
Full_droper <Full_droper@protonmail.com>
2023-02-07 18:36:22 +0200

    init

---
+++ src/run.js
@@ -0,0 +1,40 @@
+// application runner
+(async () => {
+  // configure env
+  process.env.NODE_ENV || (await require("dotenv").config({debug:false}))
+  // extract values from env
+  const { TOKEN, WS_GATEWAY, WS_VERSION, WS_ENCODING, WS_COMMPRES} = process.env;
+  // register ws Cass
+  const WebSocket = await require("ws")
+  // register event class
+  const bot = new (await require("node:events"))()
+  // open ws connection
+  const ws = new WebSocket(`wss://${WS_GATEWAY}?v=${WS_VERSION}&encoding=${WS_ENCODING}`)
+  // register payload data
+  const PAYLOAD = JSON.stringify({
+    op: 2,
+    d: {
+      token: TOKEN,
+      properties: {
+        os: 'Linux',
+        browser: 'Firefox'
+      }
+    }
+  })
+  // register connection heartbeat
+  const heartbeat = ({heartbeat_interval}) => setInterval(() => ws.send(`{"op":2,"d":null}`), heartbeat_interval);
+  // create connection hand shake
+  ws.on('open', () => ws.send(PAYLOAD));
+  // connection event processor
+  ws.on('message', data => {
+    // parse input date
+    const {t, event, op, d} = JSON.parse(data);
+    // init heartbeat
+    op === 10 && heartbeat(d);
+    // emmit events
+    bot.emit(t, d);
+    console.log(t);
+  })
+
+
+})()
